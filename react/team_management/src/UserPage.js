2f2df54825dfec11582b9a7a6bd89c8cf506e683
IgorIsailovic <igor.isailovic15@gmail.com>
2022-05-31 20:43:52 +0200

    Update UserPage.js
    Small updates

--- react/team_management/src/UserPage.js
+++ react/team_management/src/UserPage.js
@@ -1,41 +1,90 @@
 import React, { useState } from "react";
-import CssBaseline from "@mui/material/CssBaseline";
-
-import Link from "@mui/material/Link";
+import { styled, useTheme } from "@mui/material/styles";
 import Box from "@mui/material/Box";
-import Typography from "@mui/material/Typography";
-import Container from "@mui/material/Container";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
-import axios from "axios";
-import { useLocation } from "react-router-dom";
-import { maxWidth } from "@mui/system";
-
-import PropTypes from "prop-types";
-import AppBar from "@mui/material/AppBar";
-import Divider from "@mui/material/Divider";
 import Drawer from "@mui/material/Drawer";
-import IconButton from "@mui/material/IconButton";
-import List from "@mui/material/List";
-import ListItemIcon from "@mui/material/ListItemIcon";
-import GroupsIcon from "@mui/icons-material/Groups";
-import ListItemText from "@mui/material/ListItemText";
-import MenuIcon from "@mui/icons-material/Menu";
+import CssBaseline from "@mui/material/CssBaseline";
+import MuiAppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
-import ListSubheader from "@mui/material/ListSubheader";
+import List from "@mui/material/List";
+import Typography from "@mui/material/Typography";
+import Divider from "@mui/material/Divider";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
 import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import { useLocation } from "react-router-dom";
+import axios from "axios";
+import GroupsIcon from "@mui/icons-material/Groups";
+import ListSubheader from "@mui/material/ListSubheader";
 import ExpandLess from "@mui/icons-material/ExpandLess";
 import ExpandMore from "@mui/icons-material/ExpandMore";
-import StarBorder from "@mui/icons-material/StarBorder";
 import Collapse from "@mui/material/Collapse";
 import Avatar from "@mui/material/Avatar";
+import Link from "@mui/material/Link";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import Container from "@mui/material/Container";
+import { maxWidth } from "@mui/system";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
+import Card from "@mui/material/Card";
 
 const drawerWidth = 240;
 
-export default function UserPage(props) {
-  const { window } = props;
-  const [mobileOpen, setMobileOpen] = useState(false);
-  const [open, setOpen] = useState(true);
+const Main = styled("main", { shouldForwardProp: (prop) => prop !== "open" })(
+  ({ theme, open }) => ({
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    transition: theme.transitions.create("margin", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    marginLeft: `-${drawerWidth}px`,
+    ...(open && {
+      transition: theme.transitions.create("margin", {
+        easing: theme.transitions.easing.easeOut,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+      marginLeft: 0,
+    }),
+  })
+);
 
+const theme = createTheme();
+
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== "open",
+})(({ theme, open }) => ({
+  transition: theme.transitions.create(["margin", "width"], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: `${drawerWidth}px`,
+    transition: theme.transitions.create(["margin", "width"], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  }),
+}));
+
+const DrawerHeader = styled("div")(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+  justifyContent: "flex-end",
+}));
+
+export default function PersistentDrawerLeft() {
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
+  const [openSub, setOpenSub] = useState(true);
   const container =
     window !== undefined ? () => window().document.body : undefined;
 
@@ -47,13 +96,16 @@ export default function UserPage(props) {
   const iat = location.state.iat;
   const exp = location.state.exp;
 
-  console.log(`Data${data}`);
   const handleClick = () => {
-    setOpen(!open);
+    setOpenSub(!openSub);
   };
 
-  const handleDrawerToggle = () => {
-    setMobileOpen(!mobileOpen);
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
   };
 
   function handleNewTeam() {
@@ -72,224 +124,194 @@ export default function UserPage(props) {
         console.log(error);
       });
   }
-  const drawer = (
-    <div>
-      <Toolbar>
-        <Avatar alt="Remy Sharp" src="/public/logo192.png">
-          {data.firstName.charAt(0) + data.lastName.charAt(0)}
-        </Avatar>
-        <Typography
-          variant="h6"
-          component="div"
-          sx={{ flexGrow: 1 }}
-          align="center"
-        >
-          {`${data.firstName} ${data.lastName}`}
-        </Typography>
-      </Toolbar>
-      <Divider />
-      <List
-        sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}
-        component="nav"
-        aria-labelledby="nested-list-subheader"
-        subheader={
-          <ListSubheader
-            component="div"
-            id="nested-list-subheader"
-          ></ListSubheader>
-        }
-      >
-        <ListItemButton onClick={handleClick}>
-          <ListItemIcon>
-            <GroupsIcon />
-          </ListItemIcon>
 
-          <ListItemText primary="Teams" />
-          {open ? <ExpandLess /> : <ExpandMore />}
-        </ListItemButton>
-        <Collapse in={open} timeout="auto" unmountOnExit>
-          <List component="div" disablePadding>
-            {data.teamUser.map((team, key) => (
-              <ListItemButton
-                sx={{ pl: 4 }}
-                onClick={() => console.log(Date.now())}
-              >
-                <ListItemText primary={team.name} />
-              </ListItemButton>
-            ))}
-            <ListItemButton sx={{ pl: 4 }} onClick={handleNewTeam}>
-              <ListItemText primary="Join another team" />
-            </ListItemButton>
-          </List>
-        </Collapse>
-      </List>
-    </div>
-  );
+  function Copyright(props) {
+    return (
+      <Typography
+        variant="body2"
+        color="text.secondary"
+        align="center"
+        {...props}
+      >
+        {"Copyright © "}
+        <Link color="inherit" href="https://github.com/IgorIsailovic">
+          Igor Isailovic
+        </Link>{" "}
+        {new Date().getFullYear()}
+        {"."}
+      </Typography>
+    );
+  }
 
   return (
     <Box sx={{ display: "flex" }}>
       <CssBaseline />
-      <AppBar
-        position="fixed"
-        sx={{
-          width: { sm: `calc(100% - ${drawerWidth}px)` },
-          ml: { sm: `${drawerWidth}px` },
-        }}
-      >
+      <AppBar position="fixed" open={open}>
         <Toolbar>
           <IconButton
             color="inherit"
             aria-label="open drawer"
+            onClick={handleDrawerOpen}
             edge="start"
-            onClick={handleDrawerToggle}
-            sx={{ mr: 2, display: { sm: "none" } }}
+            sx={{ mr: 2, ...(open && { display: "none" }) }}
           >
             <MenuIcon />
           </IconButton>
-          <Typography variant="h6" noWrap component="div"></Typography>
+
+          <Typography
+            variant="h6"
+            component="div"
+            sx={{ flexGrow: 1 }}
+            align="center"
+          >
+            User Page
+          </Typography>
         </Toolbar>
       </AppBar>
-      <Box
-        component="nav"
-        sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
-        aria-label="mailbox folders"
-      >
-        {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
-        <Drawer
-          container={container}
-          variant="temporary"
-          open={mobileOpen}
-          onClose={handleDrawerToggle}
-          ModalProps={{
-            keepMounted: true, // Better open performance on mobile.
-          }}
-          sx={{
-            display: { xs: "block", sm: "none" },
-            "& .MuiDrawer-paper": {
-              boxSizing: "border-box",
-              width: drawerWidth,
-            },
-          }}
-        >
-          {drawer}
-        </Drawer>
-        <Drawer
-          variant="permanent"
-          sx={{
-            display: { xs: "none", sm: "block" },
-            "& .MuiDrawer-paper": {
-              boxSizing: "border-box",
-              width: drawerWidth,
-            },
-          }}
-          open
-        >
-          {drawer}
-        </Drawer>
-      </Box>
-      <Box
-        component="main"
+      <Drawer
         sx={{
-          flexGrow: 1,
-          p: 3,
-          width: { sm: `calc(100% - ${drawerWidth}px)` },
+          width: drawerWidth,
+          flexShrink: 0,
+          "& .MuiDrawer-paper": {
+            width: drawerWidth,
+            boxSizing: "border-box",
+          },
         }}
+        variant="persistent"
+        anchor="left"
+        open={open}
       >
-        <Toolbar />
+        <DrawerHeader>
+          <Avatar alt="Remy Sharp" src="/public/logo192.png">
+            {data.firstName.charAt(0) + data.lastName.charAt(0)}
+          </Avatar>
+          <Typography
+            variant="h6"
+            component="div"
+            sx={{ flexGrow: 1 }}
+            align="center"
+          >
+            {`${data.firstName} ${data.lastName}`}
+          </Typography>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === "ltr" ? (
+              <ChevronLeftIcon />
+            ) : (
+              <ChevronRightIcon />
+            )}
+          </IconButton>
+        </DrawerHeader>
+        <Divider />
+        <List
+          sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}
+          component="nav"
+          aria-labelledby="nested-list-subheader"
+          subheader={
+            <ListSubheader
+              component="div"
+              id="nested-list-subheader"
+            ></ListSubheader>
+          }
+        >
+          <ListItemButton onClick={handleClick}>
+            <ListItemIcon>
+              <GroupsIcon />
+            </ListItemIcon>
+
+            <ListItemText primary="Teams" />
+            {openSub ? <ExpandLess /> : <ExpandMore />}
+          </ListItemButton>
+          <Collapse in={openSub} timeout={1} unmountOnExit>
+            <List component="div" disablePadding>
+              {data.teamUser.map((team, key) => (
+                <ListItemButton
+                  sx={{ pl: 4 }}
+                  onClick={() => console.log(Date.now())}
+                >
+                  <ListItemText primary={team.name} />
+                </ListItemButton>
+              ))}
+              <ListItemButton sx={{ pl: 4 }} onClick={handleNewTeam}>
+                <ListItemText primary="Join another team" />
+              </ListItemButton>
+            </List>
+          </Collapse>
+        </List>
+      </Drawer>
+      <Main open={open}>
+        <DrawerHeader />
         <ThemeProvider theme={theme}>
           <Container component="main" maxWidth="xs">
             <CssBaseline />
-            <Box
-              sx={{
-                marginTop: 8,
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "center",
-              }}
-            >
-              <div>
-                <h1>Podaci o korisniku:</h1>
-                <h2>Username: {user}</h2>
-                <h2>Roles: {roles}</h2>
-
-                <h2>IAT: {iat}</h2>
-                <h2>Expiery: {exp}</h2>
-                <hr
-                  style={{
-                    color: "#000000",
-                    backgroundColor: "#000000",
-                    height: 0.5,
-                    borderColor: "#000000",
-                    width: maxWidth,
+            <h1 style={{ textAlign: "center" }}>Tasks</h1>
+            {data.taskUser.length > 0 ? (
+              data.taskUser.map((task, key) => (
+                <Box
+                  sx={{
+                    marginTop: 4,
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center",
+                    width: "100%",
                   }}
-                />
-              </div>
-            </Box>
+                >
+                  <Card
+                    //variant="outlined"
+                    style={{
+                      backgroundColor: "#1976d3",
+                      padding: "10px",
+                      width: "100%",
+                      textAlign: "center",
+                      borderRadius: "1rem",
+                    }}
+                  >
+                    <Typography
+                      variant="body1"
+                      color="white"
+                      align="center"
+                      fontWeight={900}
+                    >
+                      {task.name}
+                    </Typography>
+
+                    <Typography
+                      variant="body2"
+                      color="white"
+                      align="center"
+                      marginBottom={2}
+                    >
+                      {task.description}
+                    </Typography>
+                    <Button
+                      style={{
+                        color: "white",
+                        border: "solid 2px white",
+                      }}
+                    >
+                      Detailed
+                    </Button>
+                  </Card>
+                </Box>
+              ))
+            ) : (
+              <h1 style={{ textAlign: "center" }}>
+                Trenutno nema podataka o taskovima
+              </h1>
+            )}
             <Box
               sx={{
-                marginTop: 8,
+                marginTop: 4,
                 display: "flex",
                 flexDirection: "column",
                 alignItems: "center",
+                width: "100%",
               }}
             >
-              {data.taskUser.length > 0 ? (
-                ((<h1>Podaci o taskovima:</h1>),
-                data.taskUser.map((task, key) => (
-                  <>
-                    <h2>TASK NAME: {task.name}</h2>
-                    <h2>DESCRIPTION: {task.description}</h2>
-                    <h2>ESTIMATED DURATION:{task.est_dur}</h2>
-                    <h2>STATUS: {task.status}</h2>
-                    <hr
-                      style={{
-                        color: "#000000",
-                        backgroundColor: "#000000",
-                        height: 0.5,
-                        borderColor: "#000000",
-                        width: maxWidth,
-                      }}
-                    />
-                  </>
-                )))
-              ) : (
-                <h1 style={{ textAlign: "center" }}>
-                  Trenutno nema podataka o taskovima
-                </h1>
-              )}
+              <Copyright />
             </Box>
-            <Copyright sx={{ mt: 8, mb: 4 }} />
           </Container>
         </ThemeProvider>
-      </Box>
+      </Main>
     </Box>
   );
 }
-
-UserPage.propTypes = {
-  /**
-   * Injected by the documentation to work in an iframe.
-   * You won't need it on your project.
-   */
-  window: PropTypes.func,
-};
-
-function Copyright(props) {
-  return (
-    <Typography
-      variant="body2"
-      color="text.secondary"
-      align="center"
-      {...props}
-    >
-      {"Copyright © "}
-      <Link color="inherit" href="https://mui.com/">
-        Your Website
-      </Link>{" "}
-      {new Date().getFullYear()}
-      {"."}
-    </Typography>
-  );
-}
-
-const theme = createTheme();
